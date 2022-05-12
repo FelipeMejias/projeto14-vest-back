80fd5d620967b6da6719d32da982b44d76c9d0f0
Felipe Mejias <felipe.mejias50@gmail.com>
2022-05-12 16:38:54 -0300

    get parametro search

--- index.js
+++ index.js
@@ -45,6 +45,26 @@ app.get('/itens/:idItem', async (req,res)=>{
     }catch{res.sendStatus(499)}
 })
 
+app.get('/busca/:palavra', async (req,res)=>{
+    const {palavra}=req.params
+    
+    try{
+        const listaTodos =await db.collection('itens').find({}).toArray()
+        const listaFiltrados=listaTodos.filter((obj)=>{
+            let bool
+            const lista=obj.nome.split(' ')
+            for(let k=0;k<lista.length;k++){
+                bool=true
+                for(let w=0;w<palavra.length;w++){
+                    if(palavra[w]!=lista[k][w]){bool=false}
+                }
+                if(bool){return true}
+            }return false
+
+        })
+        res.send(listaFiltrados)
+    }catch{res.sendStatus(499)}
+})
 
 
 
